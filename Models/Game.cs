faf7066925cab94540aa3984fa5d3a2870549778
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2024-11-12 12:47:02 +0100

    updated to match client

--- Models/Game.cs
+++ Models/Game.cs
@@ -2,11 +2,17 @@
 {
     public class Game
     {
+        private int id;
+        private DateTime date;
+        private string against = String.Empty;
+        private Field field;
+        private bool home;
 
-        public int Id { get; set; }
-        public DateTime Date { get; set; }
-        public required string Against { get; set; }
-        public Field Field { get; set; }
+        public int Id { get => id; set => id = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public string Against { get => against; set => against = value; }
+        public Field Field { get => field; set => field = value; }
+        bool Home { get => home; set => home = value; }
 
     }
     public enum Field
