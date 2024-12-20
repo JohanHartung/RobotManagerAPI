daaa9b0ea7bc6e4cadffc6403f98ca1d8a8ecb11
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2024-12-20 18:19:33 +0100

    added process class

---
+++ Models/Process.cs
@@ -0,0 +1,14 @@
+﻿namespace RobotManagerAPI.Models
+{
+    public class Process
+    {
+        private int id;
+        private int userId;
+        private string? secret;
+
+        public int Id { get => id; set => id = value; }
+        public int UserId { get => userId; set => userId = value; }
+        public string? Secret { get => secret; set => secret = value; }
+
+    }
+}
