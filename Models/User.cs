3e0fdf1a5e1bab7d740c6fc4529122b8f440d0bf
Johan Hartung <johan.hartung@informatik.hu-berlin.de>
2024-12-20 18:20:17 +0100

    added User class

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RobotManagerAPI.Models
+{
+    public class User
+    {
+        private int id;
+        private string? username;
+        private string? password;
+        private Dictionary<string, string> tokens = new(); // Dictionary<deviceId, token>
+
+        public int Id { get => id; set => id = value; }
+        public string? Username { get => username; set => username = value; }
+        public string? Password { get => password; set => password = value; }
+
+        [NotMapped]
+        public Dictionary<string, string> Tokens { get => tokens; set => tokens = value; }
+    }
+}
